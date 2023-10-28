dffae3b0f686666b0cc08a132c640747f736b1ea
Agnaldo-Sites <Fernandojubilel@gmail.com>
2023-10-28 00:46:40 -0300

    2810-0046

--- index.php
+++ index.php
@@ -5,28 +5,30 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
-    <link rel="stylesheet" type="text/css" href="style.css" media="screen" />
+    <link rel="stylesheet" type="text/css" href="styles.css" media="screen" />
 </head>
 
 <body>
-    <div class="main-login">
-        <div class="right-login">
-            <div class="card-login">
-            <div class="card-login2">
-            <h1 class="MeuNome" style="color: white; line-height: 1;"><span style="color: rgb(255, 38, 0); padding-left: 0px;">Login</span></h1>
-                <div class="textfield">
+    <div class="card-login">
+        <div class="card-login2">
+            <h1 class="MeuNome" style="color: white; line-height: 1;text-align: center;"><span style="color: rgb(255, 38, 0); padding-left: 0px;">Login</span></h1>
+            <div class="textfield">
+                <div class="usuarioo">
                     <label for="usuario">Usuário</label>
-                    <input type="text" name="usuario" placeholder="Usuário">
                 </div>
-                <div class="textfield">
+                <input type="text" name="usuario" placeholder="Usuário">
+            </div>
+            <div class="textfield">
+                <div class="usuarioo">
                     <label for="senha">Senha</label>
-                    <input type="password" name="senha" placeholder="Senha">
                 </div>
-                <button class="btn-login">Entrar</button>
+                <input type="password" name="senha" placeholder="Senha">
             </div>
-            </div>
-
+            <button class="btn-login">Entrar</button>
         </div>
+    </div>
+    <div class="imgea">
+        <img class="img" src="https://i.pinimg.com/originals/12/5f/67/125f6731ffaa722d9377bec81f83dea5.gif" alt="img">
     </div>
 </body>
 
