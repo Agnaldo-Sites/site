cc2e5667f6ee1d46d1c6d12f945af18aa6a7be08
Agnaldo-Sites <Fernandojubilel@gmail.com>
2023-10-27 22:57:24 -0300

    2710-2257

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link rel="stylesheet" type="text/css" href="style.css" media="screen" />
+</head>
+
+<body>
+    <div class="main-login">
+        <div class="right-login">
+            <div class="card-login">
+            <div class="card-login2">
+            <h1 class="MeuNome" style="color: white; line-height: 1;"><span style="color: rgb(255, 38, 0); padding-left: 0px;">Login</span></h1>
+                <div class="textfield">
+                    <label for="usuario">Usuário</label>
+                    <input type="text" name="usuario" placeholder="Usuário">
+                </div>
+                <div class="textfield">
+                    <label for="senha">Senha</label>
+                    <input type="password" name="senha" placeholder="Senha">
+                </div>
+                <button class="btn-login">Entrar</button>
+            </div>
+            </div>
+
+        </div>
+    </div>
+</body>
+
+</html>
